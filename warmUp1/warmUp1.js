03b12330404baa07c39085950c927705e5c707d0
ABDELLI Houssem Eddine <abdelli.hou@gmail.com>
2020-04-13 16:39:37 +0100

    Refactored 13-04-2019

--- warmUp1/warmUp1.js
+++ warmUp1/warmUp1.js
@@ -2,107 +2,34 @@
 // 2-find if the number 13 is a multiple of 3 or not.
 // 3-calculate the average age of the follwing ages [13,14,13,15,16,17,19,13,16,15]
 // 4-calculate your age in seconds.
-
 // your code is here
-<<<<<<< HEAD
 
-<<<<<<< HEAD
-//1
-function name(firstName, lastName) {
-	return firstName + ' ' + lastName;
-}
-//'Siwar'+' Hkimi';
 
-<<<<<<< HEAD
-<<<<<<< HEAD
+
 
 //1
-var firstName = "Houssem Eddine";
-var lastName = "ABDELLI";
-var fullName = " ";
-fullName = firstName + " " + lastName
-console.log(fullName)
-
-
-
+function fullName (firstName , lastName) {
+	return firstName + " " + lastName
+}
 //2
-var x = 13;
-if (x % 3 === 0) {
-	console.log(x + "is a multiple of 3")
+function multipleOrNot (number) {
+	if (x%3===0) {
+		return x + " is a multiple of 3"
+	}
+	else return x + " is not a multiple of 3"
 }
-console.log(x + " is not a multiple of 3")
-
-
-
-
 //3
-var array = [13, 14, 13, 15, 16, 17, 19, 13, 16, 15];
-var average = 0;
-for (var i = 0; i < array.length; i++) {
-	average = (array[i] / array.length) + average;
+function average (array) {
+	var averageAge = 0;
+	for (var i=0 ; i<array.length; i++) {
+		averageAge= averageAge + array[i]/array.length
+	}
+	return averageAge
 }
-console.log(average)
-
-
-
-
 //4
-var ageInYears = 33;
-var ageInSeconds = 0;
-var x = 0;
-x = 365.25 * 24 * 60 * 60;
-ageInSeconds = x * ageInYears;
-console.log(ageInSeconds)
-
-
-=======
-=======
-//2
-function multipleOrNot(x, y) {
-	if (x % y === 0) {
-		return x + " is a multiple of " + y + ".";
-	}
-	else {
-		return x + " is not a multiple of " + y + ".";
-	}
-}
-//13%3===1 different from 0 so 13 isn't a multiple of 3
-
-//3
-function average(arr) {
-	var i = 0;
-	var average = 0;
-	while (i < arr.length) {
-		average = average + arr[i]
-		i++
-	}
-	return average / arr.length
+function calculateAgeInSeconds (ageInYears) {
+	return "your age in seconds is " + ageInYears*365.25*24*60*60
 }
 
-//average=(13+14+13+15+16+17+19+13+16+15)/10;
 
-//4
-// function ageInSeconds(day,month,year){
-// var ageYear=0;
-// var ageMonth=0;
-// var ageDay=0;
-// ageYear=(2020-year)*365*24*60;
-// var array=['January','February','March','April','May','June','July','August','Septembre','October','November','December']
-// if(month==='January'||month==='March'||month==='May'||month==='July'||month==='Septembre'||month==='Novembre'){
-// 	ageMonth=31*24*60;
-// }	
-// else if(month==='April'||month==='June'||month==='August'||month==='October'||month==='December'){
-// 	ageMonth=30*24*60;
-// }
-// else {
-// 	ageMonth=28*24*60;
-// }
-// 
-// }
-//age=26*365*24*60
-=======
 
->>>>>>> 2b098376bbe5f40b97b1dad225ca4eb6a867d63d
->>>>>>> c07720253c870302a2825e47eeb180a082689769
-=======
->>>>>>> b8a430e8ea2c4d0e1e78f81ef8a28a628ba965c6
